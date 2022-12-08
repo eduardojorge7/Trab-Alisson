b0cc32f8759905c32313eb4921bb240bfa7929c9
Leonardo Boleli <56308143+LeonardoBoleli@users.noreply.github.com>
2022-12-08 01:06:00 -0300

    leo

--- server.js
+++ server.js
@@ -44,26 +44,24 @@ app.use(session({
 
 //Rotas
     //Cadastro
-app.post('/cadastrar', async (req, res) => {
-    const usuario = await Usuario.create({
-        nome_completo: req.body.fullname,
-        username: req.body.username,
-        email: req.body.email,
-        senha: req.body.password
-    });
-    const nomeBanco = await Usuario.findOne({where: {nome_completo: req.body.fullname}});
-    const userBanco = await Usuario.findOne({where: {username: req.body.username}});
-    const emailBanco = await Usuario.findOne({where: {email: req.body.email}});
-    console.log(nomeBanco);
-    console.log(userBanco);
-    console.log(emailBanco);
-    if(!nomeBanco && !userBanco && !emailBanco){
-        req.session.usuario = usuario;
-        res.redirect('main');
-    }else{
-        res.redirect('cadastrar.html');
-    }
-})
+    app.post('/cadastrar', async (req, res) => {
+        const userBanco = await Usuario.findOne({where: {username: req.body.username}});
+        const emailBanco = await Usuario.findOne({where: {email: req.body.email}});
+        if((!userBanco) && (!emailBanco)){
+            const usuario = await Usuario.create({
+                nome_completo: req.body.fullname,
+                username: req.body.username,
+                email: req.body.email,
+                senha: req.body.password
+            });    
+        
+            req.session.usuario = usuario;
+            res.redirect('main');
+        }else{
+            console.log("usuario ja cadastrado");
+            res.redirect('cadastrar.html');
+        }
+    })
 
 
     //Login na plataforma
